1d32223629557cb5be223b69f4d31672b3eb9352
Jempsa <t2koje02@students.oamk.fi>
2024-08-31 13:43:24 +0300

    tavoitesyke

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [lowerHeartRate, setLowerHeartRate] = useState('0');
+  const [upperHeartRate, setUpperHeartRate] = useState('0');
+
+
+  const calculateHeartRate = () => {
+    const ageNum = parseInt(age);
+    if (!isNaN(ageNum)) {                     //Tarkistetaan onko ageNum numero isNaN = Jos syötetty arvo ei ole numero, palautetaan NaN (not a number)
+      const lower = (220 - ageNum) * 0.65;    // Sykkeen laskenta
+      const upper = (220 - ageNum) * 0.85;
+      setLowerHeartRate(lower.toFixed(1));    //Desinaalin arvot
+      setUpperHeartRate(upper.toFixed(1));
+    } else {
+      alert('Please enter a valid age');      // alert Jos ei syötä ikää ja painaa calculate 
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Text>Age</Text>
+      <TextInput
+        placeholder='Insert age'
+        value={age}
+        onChangeText={text => setAge(text)}
+        keyboardType='decimal-pad'
+        style={styles.input}
+      />
+
+     <Button title='Calculate' onPress={calculateHeartRate}  color="black"/>
+    <Text style={styles.field}>
+      {lowerHeartRate !== '0' && upperHeartRate !== '0' ? `Limits: ${lowerHeartRate} - ${upperHeartRate} bpm` : ''} 
+    </Text>
+    <StatusBar style="auto" />
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#CCAAEE',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 8,
+  },
+  field: {
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  input: {
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 200,
+    paddingHorizontal: 8,
+    marginVertical: 10,
   },
 });
